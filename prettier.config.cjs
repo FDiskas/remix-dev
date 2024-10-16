a84f7b50e32af8bec563c8c5ceb7ff110d87b1eb
Vytenis Kuciauskas <vytenis.kuciauskas@nfq.com>
2024-10-17 02:33:06 +0300

    Eslint is not working on css files

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,9 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import("prettier").Config} */
 module.exports = {
+  tailwindConfig: resolve(__dirname, "./tailwind.config.ts"),
   plugins: ["prettier-plugin-tailwindcss"],
 };
